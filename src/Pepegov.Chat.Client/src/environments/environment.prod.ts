f6602d918c93b766beb330ecc63bb6307c91cd97
pepegov <andelismore@gmail.com>
2024-04-04 09:45:33 +0300

    use http for api in client

--- src/Pepegov.Chat.Client/src/environments/environment.prod.ts
+++ src/Pepegov.Chat.Client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:11011/api/',
-  hubUrl: 'https://localhost:11011/hubs/',
+  apiUrl: 'http://localhost:11010/api/',
+  hubUrl: 'http://localhost:11010/hubs/',
   identityUrl : "identity-app-1"
 };
