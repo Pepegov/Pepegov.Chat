b681930a045b402557c8ec10fbe8b5570cffe001
pepegov <andelismore@gmail.com>
2024-04-07 19:57:49 +0300

    change client api env

--- src/Pepegov.Chat.Client/src/environments/environment.prod.ts
+++ src/Pepegov.Chat.Client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: '/api/',
-  hubUrl: '/hubs/',
+  apiUrl: 'http://server:11010/api/',
+  hubUrl: 'http://server:11010/hubs/',
   identityUrl : "identity-app-1"
 };
