a5b3d55262b37326a4ea5ad37b965928d50eb863
pepegov <andelismore@gmail.com>
2024-04-08 14:15:25 +0300

    update client env

--- src/Pepegov.Chat.Client/src/environments/environment.prod.ts
+++ src/Pepegov.Chat.Client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.chat.pepegov.ru/api/',
-  hubUrl: 'wss://api.chat.pepegov.ru/hubs/',
+  hubUrl: 'https://api.chat.pepegov.ru/hubs/',
   identityUrl : "https://identity.pepegov.ru/",
 };
