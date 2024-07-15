6c1213af46995b25d7778408744d33fa7a0f78c5
Pepegov <andelismore@gmail.com>
2024-07-15 11:36:41 +0300

    Uppend entries to webpack conf

--- src/Pepegov.Chat.Client.Web.Minimal/webpack.config.js
+++ src/Pepegov.Chat.Client.Web.Minimal/webpack.config.js
@@ -11,6 +11,13 @@ module.exports = (env, argv) => {
     return {
         entry: {
             index: './src/index.ts',
+            about: './src/app/pages/about.ts',
+            home: './src/app/pages/room/room.component.ts',
+            login: './src/app/pages/login/login.component.ts',
+            notFound: './src/app/pages/not-found.ts',
+            lobby: './src/app/pages/lobby/lobby.component.ts',
+            toolbar: './src/app/components/toolbar/toolbar.component.ts',
+            modal: './src/app/components/modal/modal.component.ts'
         },
         optimization: {
             runtimeChunk: true,
